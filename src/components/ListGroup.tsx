537b97012ac1e3e5854dfbd456d62284b03a6a38
prvn <karnapraveen22@gmail.com>
2024-06-26 17:05:25 +0545

    rendering component using map method and also applied conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,40 @@
+// function ListGroup() {
+//   let items = ["Kathmandu", "Bhaktapur", "Lalitpur", "Hetauda", "Chitwan"];
+//   items = [];
+//   if (items.length === 0)
+//     return (
+//       <>
+//         <h1> Lists</h1>
+//         <p>No Items Found </p>
+//       </>
+//     );
+//   return (
+//     <>
+//       <h1>List</h1>
+//       <ul className="list-group">
+//         {items.map((items) => (
+//           <li key={items}>{items}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default ListGroup;
+// above method is not a good practice as there are two headings
+//conditional rendering method 2
 function ListGroup() {
+  let items = ["Kathmandu", "Bhaktapur", "Lalitpur", "Hetauda", "Chitwan"];
+  items = [];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((items) => (
+          <li key={items}>{items}</li>
+        ))}
       </ul>
     </>
   );
