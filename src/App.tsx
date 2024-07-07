c157c0c36db2677a8917473c3cc82e21387b9c98
prvn <karnapraveen22@gmail.com>
2024-07-07 17:50:58 +0545

    created button and clickevent

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+
+import Button from "./components/Button";
+
+// function App() {
+//   let items = ["Kathmandu", "Bhaktapur", "Lalitpur", "Hetauda", "Chitwan"];
+//   const handelSelectItem = (items: string) => {
+//     console.log(items);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handelSelectItem}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  let items = ["Kathmandu", "Bhaktapur", "Lalitpur", "Hetauda", "Chitwan"];
-  const handelSelectItem = (items: string) => {
-    console.log(items);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handelSelectItem}
-      />{" "}
+      <Button color="danger" onClick={() => console.log("clicked button")}>
+        My Button
+      </Button>
     </div>
   );
 }
-
 export default App;
