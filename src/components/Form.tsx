8287fed30d7a7c1f81838e2d961c53c1552814a8
prvn <karnapraveen22@gmail.com>
2024-08-13 19:22:40 +0545

    accessing value entered in input fields using state hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,22 @@
-import React, { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
-  const nameref = useRef<HTMLInputElement>(null);
-  const ageref = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  //using ref hook
+  //   const nameref = useRef<HTMLInputElement>(null);
+  //   const ageref = useRef<HTMLInputElement>(null);
+  //   const person = { name: "", age: 0 };
+
+  // use state hook
+  const [person, setPerson] = useState({
+    name: "",
+    age: 0,
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameref.current !== null) person.name = nameref.current.value;
-    if (ageref.current !== null) person.age = parseInt(ageref.current.value);
+    // for ref hook
+    // if (nameref.current !== null) person.name = nameref.current.value;
+    // if (ageref.current !== null) person.age = parseInt(ageref.current.value);
     console.log(person);
   };
   return (
@@ -17,13 +25,35 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameref} id="name" type="text" className="form-control" />
+        {/* for ref hook */}
+        {/* <input ref={nameref} id="name" type="text" className="form-control" /> */}
+
+        {/* for state hook  */}
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageref} id="age" type="number" className="form-control" />
+        {/* for ref hook  */}
+        {/* <input ref={ageref} id="age" type="number" className="form-control" /> */}
+        {/* for state hook  */}
+
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
