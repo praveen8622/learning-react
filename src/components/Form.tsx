0c24e55dfd34b151dc2cce275194640ca398a937
prvn <karnapraveen22@gmail.com>
2024-08-15 09:37:44 +0545

    managing form with react hook form  hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,38 +1,82 @@
 import { FormEvent, useRef, useState } from "react";
+import { FieldValue, FieldValues, useForm } from "react-hook-form";
 
+// const Form = () => {
+//   //using ref hook
+//   //   const nameref = useRef<HTMLInputElement>(null);
+//   //   const ageref = useRef<HTMLInputElement>(null);
+//   //   const person = { name: "", age: 0 };
+
+//   // use state hook
+//   const [person, setPerson] = useState({
+//     name: "",
+//     age: "",
+//   });
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     // for ref hook
+//     // if (nameref.current !== null) person.name = nameref.current.value;
+//     // if (ageref.current !== null) person.age = parseInt(ageref.current.value);
+//     console.log(person);
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">
+//           Name
+//         </label>
+//         {/* for ref hook */}
+//         {/* <input ref={nameref} id="name" type="text" className="form-control" /> */}
+
+//         {/* for state hook  */}
+//         <input
+//           onChange={(event) =>
+//             setPerson({ ...person, name: event.target.value })
+//           }
+//           value={person.name}
+//           id="name"
+//           type="text"
+//           className="form-control"
+//         />
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-label">
+//           Age
+//         </label>
+//         {/* for ref hook  */}
+//         {/* <input ref={ageref} id="age" type="number" className="form-control" /> */}
+//         {/* for state hook  */}
+
+//         <input
+//           value={person.age}
+//           onChange={(event) =>
+//             setPerson({ ...person, age: event.target.value })
+//           }
+//           id="name"
+//           type="text"
+//           className="form-control"
+//         />
+//       </div>
+//       <button className="btn btn-primary" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+// using hook-form
 const Form = () => {
-  //using ref hook
-  //   const nameref = useRef<HTMLInputElement>(null);
-  //   const ageref = useRef<HTMLInputElement>(null);
-  //   const person = { name: "", age: 0 };
-
-  // use state hook
-  const [person, setPerson] = useState({
-    name: "",
-    age: 0,
-  });
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    // for ref hook
-    // if (nameref.current !== null) person.name = nameref.current.value;
-    // if (ageref.current !== null) person.age = parseInt(ageref.current.value);
-    console.log(person);
-  };
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        {/* for ref hook */}
-        {/* <input ref={nameref} id="name" type="text" className="form-control" /> */}
-
-        {/* for state hook  */}
         <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
@@ -42,14 +86,8 @@ const Form = () => {
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        {/* for ref hook  */}
-        {/* <input ref={ageref} id="age" type="number" className="form-control" /> */}
-        {/* for state hook  */}
-
         <input
-          onChange={(event) =>
-            setPerson({ ...person, age: parseInt(event.target.value) })
-          }
+          {...register("age")}
           id="name"
           type="text"
           className="form-control"
@@ -61,5 +99,4 @@ const Form = () => {
     </form>
   );
 };
-
 export default Form;
